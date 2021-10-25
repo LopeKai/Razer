f7275c9b8e881de831cebc452df3a87eb856bf5d
LopeKai <kaiqueoakley35@hotmail.com>
2021-10-25 18:48:26 -0300

    start mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,48 +5,63 @@ import './style/header.scss'
 
 export default function Header() {
     return (
-        <header className="header">
-            <div className="hamburguer"></div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link className="containerLogo" to="/">
-                            <img className="logo" src={logo} alt="Razer" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/pc"> Pc </Link>
-                    </li>
-                    <li>
-                        <Link to="/console"> Console </Link>
-                    </li>
-                    <li>
-                        <Link to="/mobile"> Mobile </Link>
-                    </li>
-                    <li>
-                        <Link to="/lifestyle">Lifestyle</Link>
-                    </li>
-                    <li>
-                        <Link to="/services"> Services </Link>
-                    </li>
-                    <li>
-                        <Link to="/community"> Community </Link>
-                    </li>
-                    <li>
-                        <Link to="/support"> Support </Link>
-                    </li>
-                    <li>
-                        <Link to="" className="containerIcon">
-                            <ion-icon className="icon" name="search-outline"></ion-icon>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="" className="containerIcon">
-                            <ion-icon className="icon" name="cart-outline"></ion-icon>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header className="header">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link className="containerLogo" to="/">
+                                <img className="logo" src={logo} alt="Razer" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/pc"> Pc </Link>
+                        </li>
+                        <li>
+                            <Link to="/console"> Console </Link>
+                        </li>
+                        <li>
+                            <Link to="/mobile"> Mobile </Link>
+                        </li>
+                        <li>
+                            <Link to="/lifestyle">Lifestyle</Link>
+                        </li>
+                        <li>
+                            <Link to="/services"> Services </Link>
+                        </li>
+                        <li>
+                            <Link to="/community"> Community </Link>
+                        </li>
+                        <li>
+                            <Link to="/support"> Support </Link>
+                        </li>
+                        <li>
+                            <Link to="" className="containerIcon">
+                                <ion-icon className="icon" name="search-outline"></ion-icon>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="" className="containerIcon">
+                                <ion-icon className="icon" name="cart-outline"></ion-icon>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            <header className="header__mobile">
+                <div className="hamburguer"></div>
+
+                <Link className="containerLogo" to="/">
+                    <img className="logo" src={logo} alt="Razer" />
+                </Link>
+
+                <Link to="" className="containerIcon">
+                    <ion-icon className="icon" name="cart-outline"></ion-icon>
+                </Link>
+
+            </header>
+
+        </>
     )
 }
