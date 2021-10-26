2b9a533c75584a8761ea02bc17745a373b8d25bf
LopeKai <kaiqueoakley35@hotmail.com>
2021-10-26 18:43:18 -0300

    menu finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,10 @@ import logo from '../../image/logo.svg'
 import './style/header.scss'
 
 export default function Header() {
+    function buttonHambuguer() {
+        let button = document.getElementById('hamburguer')
+        button.classList.toggle('hambuguer-active')
+    }
     return (
         <>
             <header className="header">
@@ -50,7 +54,7 @@ export default function Header() {
             </header>
 
             <header className="header__mobile">
-                <div className="hamburguer"></div>
+                <div className="hamburguer" id="hamburguer" onClick={buttonHambuguer}></div>
 
                 <Link className="containerLogo" to="/">
                     <img className="logo" src={logo} alt="Razer" />
@@ -59,6 +63,35 @@ export default function Header() {
                 <Link to="" className="containerIcon">
                     <ion-icon className="icon" name="cart-outline"></ion-icon>
                 </Link>
+
+                <div className="header__mobile-containerMenu">
+                    <input className="input" type="text" placeholder="Search Razer.com" />
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/pc"> Pc </Link>
+                            </li>
+                            <li>
+                                <Link to="/console"> Console </Link>
+                            </li>
+                            <li>
+                                <Link to="/mobile"> Mobile </Link>
+                            </li>
+                            <li>
+                                <Link to="/lifestyle">Lifestyle</Link>
+                            </li>
+                            <li>
+                                <Link to="/services"> Services </Link>
+                            </li>
+                            <li>
+                                <Link to="/community"> Community </Link>
+                            </li>
+                            <li>
+                                <Link to="/support"> Support </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
             </header>
 
